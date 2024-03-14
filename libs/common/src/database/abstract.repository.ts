43ca8b923f822069f8545d5ef0bd4b23b34ea541
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2024-03-15 00:12:57 +0530

    chore: abstract repository completed

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,17 +1,60 @@
-import { Logger } from '@nestjs/common';
+import { Logger, NotFoundException } from '@nestjs/common';
 import { AbstractDocument } from './abstract.schema';
-import { Model, Types } from 'mongoose';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 
 export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   protected abstract readonly logger: Logger;
 
   constructor(protected readonly model: Model<TDocument>) {}
 
+  // Create Document
   async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
     const createdDocument = new this.model({
       ...document,
       _id: new Types.ObjectId(),
     });
     return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+
+  // Finds only one Document
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model
+      .findOne(filterQuery)
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document was not found');
+    }
+    return document;
+  }
+
+  // Finds only one Document and Update it
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, { new: true })
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document was not found');
+    }
+    return document;
+  }
+
+  // Finds multiple Documents
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    const document = await this.model.find(filterQuery).lean<TDocument[]>(true);
+    return document;
+  }
+
+  // Finds only one Document and Delete it
+  async findOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    return await this.model.findOneAndDelete(filterQuery).lean<TDocument>(true);
   }
 }
